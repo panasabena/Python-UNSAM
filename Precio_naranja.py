d87da05c57e1ac5b48746a5ce9cf8b5278ee9a7e
panasabena <62650626+panasabena@users.noreply.github.com>
2021-09-02 00:53:22 -0300

    Delete Precio_naranja.py

--- Precio_naranja.py
+++
@@ -1,7 +0,0 @@
-## Data precios
-data = open(r'C:\Users\Alfredo\Bitácoras\Python UNSAM\ejercicios_python\Data\precios.csv','rt')
-
-for i in data:
-    row=i.split(',')
-    if row[0]=='Naranja':
-        print('El precio de la naranja es: ',row[1])
