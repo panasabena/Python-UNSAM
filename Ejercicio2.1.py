bdc82b0bed53e8eff33dc15d6e979e46a12d41d0
panasabena <62650626+panasabena@users.noreply.github.com>
2021-09-02 00:52:16 -0300

    Delete Ejercicio2.1.py

--- Ejercicio2.1.py
+++
@@ -1,4 +0,0 @@
-## Ejercicio 2.1, abriendo un archivo
-with open (r'C:\Users\Alfredo\Bitácoras\Python UNSAM\Github UNSAM\Programacion_en_Python_UNSAM-master\Notas\ejercicios_python\Data\camion.csv','rt') as f:
-	data = f.read()
-print(data)
