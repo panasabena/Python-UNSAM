f8c393815b8f0313961a36c21b09c78679a310f8
panasabena <62650626+panasabena@users.noreply.github.com>
2021-10-06 23:51:37 -0300

    Add files via upload

---
+++ Modulo 6/Ejercicio 6.3.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  6 23:50:16 2021
+
+@author: Alfredo
+"""
+
+## Ejercicio 6.3
+
+def trad2s(l):
+    '''traduce una lista con 1,0 y -1 
+    a una cadena con 'f', 'o' y 'x' '''
+    d={1:'f', 0 :'o', -1:'x'}
+    s=''.join([d[c] for c in l])
+    return s
+
+def trad2l(ps):
+    '''traduce cadena con 'f', 'o' y 'x'
+    a una lista con 1,0 y -1'''
+    inv_d={'f':1, 'o':0, 'x':-1}
+    l = [inv_d[c] for c in ps]
+    return l
+
+def propagar(l, debug = True):
+    s = trad2s(l)
+    if debug:
+        print(s)#, end = ' -> ')
+    W=s.split('x')
+    PW=[w if ('f' not in w) else 'f'*len(w) for w in W]
+    ps=''.join(PW)
+    if debug:
+        print(ps)
+    return trad2l(ps)
+
+#%%
+l = [0,0,0,-1,1,0,0,0,-1,0,1,0,0]
+lp = propagar(l)
+print("Estado original:  ",l)
+print("Estado propagado: ",lp)
