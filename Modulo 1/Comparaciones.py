eb13bc7d6f153118db9e66770db59377563d4526
panasabena <62650626+panasabena@users.noreply.github.com>
2022-02-28 22:52:06 -0300

    Add files via upload

---
+++ Modulo 1/Comparaciones.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 28 22:45:12 2022
+
+@author: Alfredo
+"""
+
+a=1
+b=3
+c=5
+
+if b >= a and b <= c:
+    print ('b esta entre a y c')
+
+if not(b < a or b > c):
+    print('b sigue estando entre a y c')    
